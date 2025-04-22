00b910615b6c86b4ea3860a6e9d206bcc3c11b47
mhjanuri <hendrijanuri@gmail.com>
2025-04-22 17:26:08 +0800

    complete exercise 01 and 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR'
+    }
+    let i = 0;
+    let arr = [];
+    while (i < num) {
+        arr.push(string);
+        i++;
+    }
+    return arr.join("");
 };
+
+// console.log(repeatString('hey', 3));
 
 // Do not edit below this line
 module.exports = repeatString;
