fbcb6c14dcc164651f49f846c8e6d16bd5cb373b
mhjanuri <hendrijanuri@gmail.com>
2025-04-30 20:51:45 +0800

    plindrom

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,18 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let arr = [];
+    for (let i = 0; i < str.length; i++) {
+        let char = str [i]
+        
+        if (/[a-zA-Z0-9]/.test(char)) {
+            arr.push(char.toLowerCase());
+        }
+    }
+    let reverse = arr.slice().reverse().join("");
+    return arr.join("") === reverse;
 };
+
+// console.log(palindromes('Lid off a daffodil'));
+
 
 // Do not edit below this line
 module.exports = palindromes;
