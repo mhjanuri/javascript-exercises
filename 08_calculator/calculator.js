d24c72da888f6b109724e320b0e52eb5d1596382
mhjanuri <hendrijanuri@gmail.com>
2025-04-30 18:45:21 +0800

    smooth operator

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,33 @@
-const add = function() {
-	
+const add = function(a, b) {
+  return a + b; 
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	return array.reduce(function (a, b) {
+    return a + b;
+  }, 0);
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  return array.reduce(function (a, b) {
+    return a * b;
+  });
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return Math.pow(a, b);
 };
 
-const factorial = function() {
-	
+const factorial = function(n) {
+	let res = 1;
+    for (let i = 1; i <= n; i++) {
+        res *= i;
+    }
+    return res;
 };
 
 // Do not edit below this line
@@ -31,3 +39,9 @@ module.exports = {
   power,
   factorial
 };
+
+// console.log(sum([1, 3, 5, 7, 9]));
+// console.log(multiply([2, 4, 6, 8, 10, 12, 14]));
+// console.log(power(4, 3));
+
+
